1d60fbbf089a146dabf42efb1eecd8d93d321933
roygalet <roygalet@icloud.com>
2016-08-06 12:52:42 +0930

    Added Dust Analyzer Activity

--- app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
+++ app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
@@ -1,7 +1,11 @@
 package com.roygalet.www.solarnt;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView yesCard = (CardView) findViewById(R.id.mainCardYes);
+        yesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickYes();
+            }
+        });
+    }
+
+    public void onClickYes() {
+        Intent intent = new Intent(this, YesActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/roygalet/www/solarnt/YesActivity.java
+++ app/src/main/java/com/roygalet/www/solarnt/YesActivity.java
@@ -1,7 +1,13 @@
 package com.roygalet.www.solarnt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class YesActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class YesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yes);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("SolarNT");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CardView dustAnalyzer = (CardView) findViewById(R.id.yesCardDustAnalyzer);
+        dustAnalyzer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDustAnalyzer();
+            }
+        });
     }
+
+    private void showDustAnalyzer(){
+        Intent intent = new Intent(this,DustAnalyzer.class);
+        startActivity(intent);
+    }
+
 }
