7fc46a89e5b6ee678cb648a620c064bbd59c00be
roygalet <roygalet@icloud.com>
2016-08-06 14:15:24 +0930

    Fill Dust Analyzer Image Width and crop height

--- app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
+++ app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
@@ -2,6 +2,7 @@ package com.roygalet.www.solarnt;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DustAnalyzer extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
+    private static final int SELECT_PICTURE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,14 @@ public class DustAnalyzer extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton buttonGallery = (FloatingActionButton) findViewById(R.id.dustButtonGallery);
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+            }
+        });
+
     }
 
     private void onClickCamera(){
@@ -37,12 +48,32 @@ public class DustAnalyzer extends AppCompatActivity {
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
+    private void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = (ImageView) findViewById(R.id.dustImgImage);
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+        TextView textNoImageSelected = (TextView) findViewById(R.id.dustTextNoImage);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    imageView.setImageURI(selectedImageUri);
+                    textNoImageSelected.setText("");
+                }
+            }
+            if (requestCode == CAMERA_REQUEST) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(photo);
+                textNoImageSelected.setText("");
+            }
         }
+
     }
 
 }
