9e57c8f203d2693ba520fa62527cbcb21aa195d3
roygalet <roygalet@icloud.com>
2016-08-06 13:39:53 +0930

    Added Camera Function Working

--- app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
+++ app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
@@ -1,13 +1,17 @@
 package com.roygalet.www.solarnt;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class DustAnalyzer extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,29 @@ public class DustAnalyzer extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton buttonCamera = (FloatingActionButton) findViewById(R.id.dustButtonCamera);
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickCamera();
+            }
+        });
 
+    }
+
+    private void onClickCamera(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = (ImageView) findViewById(R.id.dustImgImage);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 
 }
