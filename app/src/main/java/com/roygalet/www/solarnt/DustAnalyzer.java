e8446d21c036af6865c4c286e621ac39e14262ae
roygalet <roygalet@icloud.com>
2016-08-06 16:21:19 +0930

    Resolved loading images as Bitmap

--- app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
+++ app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
@@ -1,9 +1,12 @@
 package com.roygalet.www.solarnt;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +15,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 public class DustAnalyzer extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int SELECT_PICTURE = 100;
+    Bitmap photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +66,7 @@ public class DustAnalyzer extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = (ImageView) findViewById(R.id.dustImgImage);
         TextView textNoImageSelected = (TextView) findViewById(R.id.dustTextNoImage);
+        TextView textPercent = (TextView) findViewById(R.id.dustTextPercent);
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 // Get the url from data
@@ -65,15 +74,32 @@ public class DustAnalyzer extends AppCompatActivity {
                 if (null != selectedImageUri) {
                     imageView.setImageURI(selectedImageUri);
                     textNoImageSelected.setText("");
+                    try {
+                        photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    textPercent.setText(Double.toString(analyzePhoto(photo)));
                 }
             }
             if (requestCode == CAMERA_REQUEST) {
-                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                photo = (Bitmap) data.getExtras().get("data");
                 imageView.setImageBitmap(photo);
                 textNoImageSelected.setText("");
+                textPercent.setText(Double.toString(analyzePhoto(photo)));
             }
         }
 
+    }
+
+    private double analyzePhoto(Bitmap photo){
+        double dust;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        dust = ByteBuffer.wrap(byteArray).getDouble();
+        System.out.println(dust);
+        return dust;
     }
 
 }
