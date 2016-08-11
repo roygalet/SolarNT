d71706379fde7fbdbb55bd3695ef6a5fb39ac9a1
roygalet <roygalet@icloud.com>
2016-08-12 03:54:48 +0930

    Add PostCode-Suburb Auto-Complete list

--- app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
+++ app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
@@ -13,6 +13,8 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private WeatherList weatherList;
+    private String[] suburbs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
                     while ((line = reader.readLine()) != null) {
                         String[] data = line.split(",");
                         weatherList.addWeatherData(Integer.parseInt(data[0]), "0".concat(data[1]), data[2], Float.parseFloat(data[3]), Float.parseFloat(data[4]), Integer.parseInt(data[5]), data[6], Float.parseFloat(data[7]), Float.parseFloat(data[8]), Float.parseFloat(data[9]), Float.parseFloat(data[10]), Integer.parseInt(data[11]), data[12], Float.parseFloat(data[13]), Float.parseFloat(data[14]), Float.parseFloat(data[15]), Float.parseFloat(data[16]), Integer.parseInt(data[17]), data[18], Float.parseFloat(data[19]), Float.parseFloat(data[20]), Float.parseFloat(data[21]), Float.parseFloat(data[22]), Float.parseFloat(data[23]), Float.parseFloat(data[24]), Float.parseFloat(data[25]));
+                    }
+                    suburbs = new String[weatherList.getCount()];
+                    for(int index = 0; index < suburbs.length; index++){
+                        suburbs[index] = weatherList.getWeatherDataByIndex(index).getPostcode().concat(weatherList.getWeatherDataByIndex(index).getSuburb());
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
--- app/src/main/java/com/roygalet/www/solarnt/WeatherList.java
+++ app/src/main/java/com/roygalet/www/solarnt/WeatherList.java
@@ -7,15 +7,23 @@ import java.util.List;
  * Created by Roy on 12-Aug-16.
  */
 public class WeatherList {
-    ArrayList<WeatherData> weatherList;
+    private ArrayList<WeatherData> weatherList;
 
     public WeatherList(){
         weatherList = new ArrayList<WeatherData>();
     }
 
+    public int getCount(){
+        return weatherList.size();
+    }
+
     public void addWeatherData(int id, String postcode, String suburb, float latitude, float longitude, int solarstation, String solarname, float solardistance, float solarwet, float solardry, float solarmean, int rainstation, String rainname, float raindistance, float rainwet, float raindry, float rainmean, int tempstation, String tempname, float tempdistance, float tempmaxwet, float tempmaxdry, float tempmaxmean, float tempminwet, float tempmindry, float tempminmean){
         WeatherData newWeatherData = new WeatherData(id, postcode, suburb, latitude, longitude, solarstation, solarname, solardistance, solarwet, solardry, solarmean, rainstation, rainname, raindistance, rainwet, raindry, rainmean, tempstation, tempname, tempdistance, tempmaxwet, tempmaxdry, tempmaxmean, tempminwet, tempmindry, tempminmean);
         weatherList.add(newWeatherData);
+    }
+
+    public WeatherData getWeatherDataByIndex(int index){
+        return weatherList.get(index);
     }
 
     public WeatherData getWeatherDataByID(int id){
