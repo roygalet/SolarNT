a6d5bc7eff220699ec2a8c7ba4d33a3330e898cb
roygalet <roygalet@icloud.com>
2016-08-12 02:48:19 +0930

    Add Weather List

---
+++ app/src/main/java/com/roygalet/www/solarnt/WeatherList.java
@@ -0,0 +1,42 @@
+package com.roygalet.www.solarnt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Roy on 12-Aug-16.
+ */
+public class WeatherList {
+    ArrayList<WeatherData> weatherList;
+
+    public WeatherList(){
+        weatherList = new ArrayList<WeatherData>();
+    }
+
+    public void addWeatherData(int id, String postcode, String suburb, float latitude, float longitude, int solarstation, String solarname, float solardistance, float solarwet, float solardry, float solarmean, int rainstation, String rainname, float raindistance, float rainwet, float raindry, float rainmean, int tempstation, String tempname, float tempdistance, float tempmaxwet, float tempmaxdry, float tempmaxmean, float tempminwet, float tempmindry, float tempminmean){
+        WeatherData newWeatherData = new WeatherData(id, postcode, suburb, latitude, longitude, solarstation, solarname, solardistance, solarwet, solardry, solarmean, rainstation, rainname, raindistance, rainwet, raindry, rainmean, tempstation, tempname, tempdistance, tempmaxwet, tempmaxdry, tempmaxmean, tempminwet, tempmindry, tempminmean);
+        weatherList.add(newWeatherData);
+    }
+
+    public WeatherData getWeatherDataByID(int id){
+        WeatherData weatherData = null;
+        for (int index = 0; index < weatherList.size(); index++){
+            if(weatherList.get(index).getId() == id){
+                weatherData = (WeatherData) weatherList.get(index);
+                break;
+            }
+        }
+        return weatherData;
+    }
+
+    public WeatherData getWeatherDataByPostCodeAndSuburb(String postcode, String suburb){
+        WeatherData weatherData = null;
+        for (int index = 0; index < weatherList.size(); index++){
+            if(weatherList.get(index).getPostcode().compareToIgnoreCase(postcode) == 0 && weatherList.get(index).getSuburb().compareToIgnoreCase(suburb) == 0){
+                weatherData = (WeatherData) weatherList.get(index);
+                break;
+            }
+        }
+        return weatherData;
+    }
+}
