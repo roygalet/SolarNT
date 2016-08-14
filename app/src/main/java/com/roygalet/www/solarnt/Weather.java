664cffcb20fa7bf1e29555c47919228b9418a986
roygalet <roygalet@icloud.com>
2016-08-14 11:39:23 +0930

    Set WeatherData as Parcelable

--- app/src/main/java/com/roygalet/www/solarnt/Weather.java
+++ app/src/main/java/com/roygalet/www/solarnt/Weather.java
@@ -3,6 +3,7 @@ package com.roygalet.www.solarnt;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class Weather extends AppCompatActivity {
 
@@ -14,6 +15,11 @@ public class Weather extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle bundle = getIntent().getExtras();
+        WeatherData weatherData = bundle.getBundle("weather").getParcelable("weather");
+
+        ((TextView) findViewById(R.id.weatherSuburb)).setText(weatherData.getSuburb());
     }
 
 }
--- app/src/main/java/com/roygalet/www/solarnt/WeatherData.java
+++ app/src/main/java/com/roygalet/www/solarnt/WeatherData.java
@@ -278,6 +278,73 @@ public class WeatherData implements Parcelable{
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeStringArray(new String[] {String.valueOf(this.id), this.postcode, this.suburb, String.valueOf(this.latitude), String.valueOf(this.longitude), String.valueOf(this.solarstation), this.solarname, String.valueOf(this.solardistance), String.valueOf(this.solarwet), String.valueOf(this.solardry), String.valueOf(this.solarmean), String.valueOf(this.rainstation), this.rainname, String.valueOf(this.raindistance), String.valueOf(this.rainwet), String.valueOf(this.raindry), String.valueOf(this.rainmean), String.valueOf(this.tempstation), this.tempname, String.valueOf(this.tempdistance), String.valueOf(this.tempmaxwet), String.valueOf(this.tempmaxdry), String.valueOf(this.tempmaxmean), String.valueOf(this.tempminwet), String.valueOf(this.tempmindry), String.valueOf(this.tempminmean)});
+        dest.writeInt(this.id);
+        dest.writeString(postcode);
+        dest.writeString(suburb);
+        dest.writeFloat(latitude);
+        dest.writeFloat(longitude);
+        dest.writeInt(solarstation);
+        dest.writeString(solarname);
+        dest.writeFloat(solardistance);
+        dest.writeFloat(solarwet);
+        dest.writeFloat(solardry);
+        dest.writeFloat(solarmean);
+        dest.writeFloat(rainstation);
+        dest.writeString(rainname);
+        dest.writeFloat(raindistance);
+        dest.writeFloat(rainwet);
+        dest.writeFloat(raindry);
+        dest.writeFloat(rainmean);
+        dest.writeInt(tempstation);
+        dest.writeString(tempname);
+        dest.writeFloat(tempdistance);
+        dest.writeFloat(tempmaxwet);
+        dest.writeFloat(tempmaxdry);
+        dest.writeFloat(tempmaxmean);
+        dest.writeFloat(tempminwet);
+        dest.writeFloat(tempmindry);
+        dest.writeFloat(tempminmean);
+    }
 
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public WeatherData createFromParcel(Parcel in) {
+            return new WeatherData(in);
+        }
+
+        @Override
+        public Object[] newArray(int size) {
+            return new Object[0];
+        }
+
+    };
+
+    public WeatherData(Parcel parcel){
+        this.id = parcel.readInt();
+        this.postcode =  parcel.readString();
+        this.suburb = parcel.readString();
+        this.latitude = parcel.readFloat();
+        this.longitude = parcel.readFloat();
+        this.solarstation =  parcel.readInt();
+        this.solarname = parcel.readString();
+        this.solardistance = parcel.readFloat();
+        this.solarwet = parcel.readFloat();
+        this.solardry = parcel.readFloat();
+        this.solarmean = parcel.readFloat();
+        this.rainstation =  parcel.readInt();
+        this.rainname = parcel.readString();
+        this.raindistance = parcel.readFloat();
+        this.rainwet = parcel.readFloat();
+        this.raindry = parcel.readFloat();
+        this.rainmean = parcel.readFloat();
+        this.tempstation =  parcel.readInt();
+        this.tempname = parcel.readString();
+        this.tempdistance = parcel.readFloat();
+        this.tempmaxwet = parcel.readFloat();
+        this.tempmaxdry = parcel.readFloat();
+        this.tempmaxmean = parcel.readFloat();
+        this.tempminwet = parcel.readFloat();
+        this.tempmindry = parcel.readFloat();
+        this.tempminmean = parcel.readFloat();
     }
 }
