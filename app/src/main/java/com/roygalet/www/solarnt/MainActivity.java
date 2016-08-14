787f4a273e746ffddb1ca1826d6911b5313eba63
roygalet <roygalet@icloud.com>
2016-08-14 10:30:55 +0930

    Weather Data Corrctly Labelled

--- app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
+++ app/src/main/java/com/roygalet/www/solarnt/MainActivity.java
@@ -68,6 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 (findViewById(R.id.mainCardStats)).setVisibility(View.GONE);
             }
         });
+
+        ((TextView)findViewById(R.id.mainTextMoreWeatherInformation)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Weather.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void loadSuburbs(){
---
+++ app/src/main/java/com/roygalet/www/solarnt/Weather.java
@@ -0,0 +1,19 @@
+package com.roygalet.www.solarnt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
