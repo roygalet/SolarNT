063583107d670fde437b4ddc0b746fcc77c713e4
roygalet <roygalet@icloud.com>
2016-08-15 00:25:08 +0930

    Make Dust Analyzer Floating Buttons Bigger

--- app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
+++ app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
@@ -1,28 +1,20 @@
 package com.roygalet.www.solarnt;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.math.BigDecimal;
 
 public class DustAnalyzer extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
@@ -84,14 +76,15 @@ public class DustAnalyzer extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    textPercent.setText(Double.toString(analyzePhoto(photo)));
+                    textPercent.setText(String.valueOf(BigDecimal.valueOf(Double.valueOf(String.valueOf(analyzePhoto(photo)))).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue()).concat(" %"));
                 }
             }
             if (requestCode == CAMERA_REQUEST) {
                 photo = (Bitmap) data.getExtras().get("data");
                 imageView.setImageBitmap(photo);
                 textNoImageSelected.setText("");
-                textPercent.setText(Double.toString(analyzePhoto(photo)));
+                textPercent.setText(String.valueOf(BigDecimal.valueOf(Double.valueOf(String.valueOf(analyzePhoto(photo)))).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue()).concat(" %"));
+//                textPercent.setText(Double.toString(analyzePhoto(photo)));
             }
         }
 
