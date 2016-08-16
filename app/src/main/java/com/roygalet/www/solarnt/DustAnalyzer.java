0f7fbf2df389a97b38f19ce706e77e66edaa91d0
roygalet <roygalet@icloud.com>
2016-08-17 04:46:17 +0930

    Add Progress Dialog to Dust Analyzer

--- app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
+++ app/src/main/java/com/roygalet/www/solarnt/DustAnalyzer.java
@@ -1,9 +1,13 @@
 package com.roygalet.www.solarnt;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
@@ -13,7 +17,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -23,6 +26,8 @@ public class DustAnalyzer extends AppCompatActivity {
     private static final int SELECT_PICTURE = 100;
     Bitmap photo;
     WeatherData weatherData;
+    ContentValues values;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +62,20 @@ public class DustAnalyzer extends AppCompatActivity {
                 openImageChooser();
             }
         });
-
     }
 
     private void onClickCamera() throws InterruptedException {
-        photo = null;
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+//        photo = null;
+//        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, CAMERA_REQUEST);
     }
 
     private void openImageChooser() {
@@ -73,124 +85,116 @@ public class DustAnalyzer extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
     }
 
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             ImageView imageView = (ImageView) findViewById(R.id.dustImgImage);
-            TextView textNoImageSelected = (TextView) findViewById(R.id.dustTextNoImage);
-            TextView textPercent = (TextView) findViewById(R.id.dustTextPercent);
-            Toast.makeText(this, "Processing image ... Please wait", Toast.LENGTH_LONG);
+            ((TextView)findViewById(R.id.dustTextNoImage)).setVisibility(View.INVISIBLE);
             if (requestCode == SELECT_PICTURE) {
                 // Get the url from data
                 Uri selectedImageUri = data.getData();
                 if (null != selectedImageUri) {
                     imageView.setImageURI(selectedImageUri);
-                    textNoImageSelected.setText("");
                     try {
                         photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImageUri);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    Toast.makeText(this, "Processing image ... Please wait", Toast.LENGTH_LONG);
-                    textPercent.setText(String.valueOf(BigDecimal.valueOf(Double.valueOf(String.valueOf(analyzePhoto(photo)))).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue()).concat(" %"));
                 }
             }
             if (requestCode == CAMERA_REQUEST) {
-                photo = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(photo);
-//                BitmapDrawable ob = new BitmapDrawable(getResources(), photo);
-//                ((RelativeLayout)findViewById(R.id.dustRelativeLayout)).setBackground(ob);
-                textNoImageSelected.setText("");
+//                photo = (Bitmap) data.getExtras().get("data");
+//                imageView.setImageBitmap(photo);
 
-                textPercent.setText(String.valueOf(BigDecimal.valueOf(Double.valueOf(String.valueOf(analyzePhoto(photo)))).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue()).concat(" %"));
-//                textPercent.setText(Double.toString(analyzePhoto(photo)));
-            }
-        }
-
-    }
-
-    private double analyzePhoto(Bitmap inputPhoto){
-//        double dust=0;
-//
-//        int width = inputPhoto.getWidth();
-//        int height = inputPhoto.getHeight();
-//
-//        Bitmap bmpMonochrome = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-//        Canvas canvas = new Canvas(bmpMonochrome);
-//        ColorMatrix ma = new ColorMatrix();
-//        ma.setSaturation(0);
-//        Paint paint = new Paint();
-//        paint.setColorFilter(new ColorMatrixColorFilter(ma));
-//        canvas.drawBitmap(inputPhoto, 0, 0, paint);
-//
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                int pixel = bmpMonochrome.getPixel(x, y);
-//                int lowestByte = pixel & 0xff;
-//                if(lowestByte >= 97 && pixel <= 203){
-//                    dust = dust + 1;
-//                }
-//            }
-//        }
-//
-//        dust = dust / (width * height) * 100;
-//        dust = 1.1469*dust - 15.657;
-////        dust = dust + (0.104 * dust - 6.7006);
-//        if(dust < 0)dust=0;
-//        if(dust > 100)dust=100;
-        ((LinearLayout)findViewById(R.id.dustLinearLayout)).setVisibility(View.VISIBLE);
-        return getDustPixelsHSV(inputPhoto);
-    }
-
-    private double getDustPixelsHSV(Bitmap inputPhoto){
-        double dust=0;
-        float [] hsv = new float[3];
-
-        int width = inputPhoto.getWidth();
-        int height = inputPhoto.getHeight();
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int pixel = inputPhoto.getPixel(x, y);
-                Color.colorToHSV(pixel,hsv);
-                if(hsv[0]<90 && hsv[1] > 0.2){
-                    dust = dust + 1;
+                try {
+                    photo = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+                    imageView.setImageBitmap(photo);
+//                    imgView.setImageBitmap(thumbnail);
+//                    imageurl = getRealPathFromURI(imageUri);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+            new Analyzer().execute(photo);
         }
-        dust = dust / (width * height) * 100;
-        double powerLoss = weatherData.getSolarmean() * 4.5 * 1.001635544 * dust/100;
-        double cost = powerLoss * 0.2595;
-        ((TextView)findViewById(R.id.dustMessage)).setText("A fraction of the Solar Exposure is being blocked by dust resulting to lower than usual power conversion efficiency of the solar PV system."
-                .concat("\n\nA 4.5 kW PV system could lose up to ")
-                .concat(String.valueOf(BigDecimal.valueOf(powerLoss).setScale(1, BigDecimal.ROUND_HALF_UP))
-                        .concat(" kWh amounting to $ ")
-                        .concat(String.valueOf(BigDecimal.valueOf(cost).setScale(2, BigDecimal.ROUND_HALF_UP)))
-                .concat(" daily.\n\nCleaning may improve system efficiency.\n\n")));
-        return dust;
+
     }
 
-    private double getDustPixelsRGB(Bitmap inputPhoto){
-        double dust=0;
-        int iRed=0;
-        int iGreen=0;
-        int iBlue=0;
+    private class Analyzer extends AsyncTask<Bitmap, Double, Double>{
+        private ProgressDialog pd;
+        private int width;
+        private int height;
+        private int divisor;
 
-        int width = inputPhoto.getWidth();
-        int height = inputPhoto.getHeight();
+        @Override
+        protected Double doInBackground(Bitmap... inputPhoto) {
+            double dust=0;
+            float [] hsv = new float[3];
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int pixel = inputPhoto.getPixel(x, y);
-                iRed = Color.red(pixel);
-                iGreen = Color.green(pixel);
-                iBlue = Color.blue(pixel);
-                if(iRed >= 75 && iRed <= 145 && iGreen>=95 && iGreen <=155 && iBlue >=80 && iBlue <= 140){
-                    dust = dust + 1;
+            width = inputPhoto[0].getWidth();
+            height = inputPhoto[0].getHeight();
+            divisor = height/10;
+            pd.setTitle("Processing Photo (".concat(String.valueOf(width)).concat(" x ").concat(String.valueOf(height).concat(") pixels.")));
+
+            for (int y = 0; y < height; y++) {
+                if(y%divisor==1)pd.setProgress((100 * y / height));
+                for (int x = 0; x < width; x++) {
+//                    pd.setProgress((100 * y / height));
+                    int pixel = inputPhoto[0].getPixel(x, y);
+                    Color.colorToHSV(pixel,hsv);
+                    if(hsv[0]<90 && hsv[1] > 0.2){
+                        dust = dust + 1;
+                    }
                 }
             }
+            dust = dust / (width * height) * 100;
+            return dust;
         }
-        dust = dust / (width * height) * 100;
-        return dust;
-    }
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ((LinearLayout)findViewById(R.id.dustLinearLayout)).setVisibility(View.INVISIBLE);
+            pd = new ProgressDialog(DustAnalyzer.this);
+            pd.setTitle("Processing Photo");
+            pd.setMessage("Please wait.");
+            pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            pd.setCancelable(false);
+            pd.setMax(100);
+            pd.show();
+        }
+
+        @Override
+        protected void onPostExecute(Double aDouble) {
+            super.onPostExecute(aDouble);
+            double powerLoss = weatherData.getSolarmean() * 4.5 * 1.001635544 * aDouble/100;
+            double cost = powerLoss * 0.2595;
+            if (pd!=null) {
+                pd.dismiss();
+            }
+            ((LinearLayout)findViewById(R.id.dustLinearLayout)).setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.dustMessage)).setText("A fraction of the Solar Exposure is being blocked by dust resulting to lower than usual power conversion efficiency of the solar PV system."
+                    .concat("\n\nA 4.5 kW PV system in ").concat(weatherData.getSuburb()).concat(" could lose up to ")
+                    .concat(String.valueOf(BigDecimal.valueOf(powerLoss).setScale(1, BigDecimal.ROUND_HALF_UP))
+                            .concat(" kWh amounting to $ ")
+                            .concat(String.valueOf(BigDecimal.valueOf(cost).setScale(2, BigDecimal.ROUND_HALF_UP)))
+                            .concat("* daily.\n\nCleaning may improve system efficiency.\n\n")));
+            ((TextView)findViewById(R.id.dustTextPercent)).setText(String.valueOf(BigDecimal.valueOf(Double.valueOf(String.valueOf(aDouble))).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue()).concat(" %"));
+        }
+
+        @Override
+        protected void onProgressUpdate(Double... values) {
+            super.onProgressUpdate(values);
+        }
+    }
 }
