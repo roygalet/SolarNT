3fe95db45aca6cf5226a74033e808d8bb9243e2b
Roy <roygalet@icloud.com>
2017-03-06 01:08:53 +0930

    Redesigned App

--- app/src/main/java/com/roygalet/www/solarnt/Settings.java
+++ app/src/main/java/com/roygalet/www/solarnt/Settings.java
@@ -190,8 +190,8 @@ public class Settings extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-//            bindPreferenceSummaryToValue(findPreference("example_text"));
-//            bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("generalPostCode"));
+            bindPreferenceSummaryToValue(findPreference("generalSuburb"));
         }
 
         @Override
